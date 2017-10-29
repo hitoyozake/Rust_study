fc66735f973d3e6c61e5f9c479a9f5d831efc080
hitoyozake <mikoto_1122@yahoo.co.jp>
2017-10-29 23:38:30 +0900

    add

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
     println!("Hello, world!");
 
-    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let whole = &a[..];
-    let part = &a[1..4];
+    let part = &a[0..4];
     let part_2 = &a[5..];
 
+    println!("whole : ");
+    for i in whole {
+        println!("{}", i);
+    }
+    println!("part:");
+    for i in part {
+        println!("{}", i);
+    }
 
+    println!("part2:");
+    for i in part_2 {
+        println!("{}", i);
+    }
 
 }
