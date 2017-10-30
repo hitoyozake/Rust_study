498ad5a2d3587cc3bd11b010a310672df7d09b04
hitoyozake <mikoto_1122@yahoo.co.jp>
2017-10-31 06:19:35 +0900

    a

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -10,30 +10,27 @@ fn main() {
     let path = Path::new("hello.csv");
     let display = path.display();
 
-    let mut file = match File.open(path){
+    let mut file = match File::open(path){
         Ok(file) => file,
         Err(why) => panic!("couldn't open {} : {}", display, why),
-    }
+    };
     
     let mut str = String::new();
     let mut count = 0; //ヘッダを読み飛ばす場合に必要
     let skip_header = false;
-    let mut output = Vec< Vec<&str> >::new();
+    
+    let mut output = Vec<Vec<&str>>::new();
     match file.read_to_string(&mut str){
 
         Err(why) => ("couldn't read contents {} : {}", display, why ),
 
         Ok(_)=>{
             if skip_header == false || count != 0 {
-                let v: Vec<&str> = s.split(',').collect();
-
-
+                let v: Vec<&str> = str.split(',').collect();
             }
-
-            ++count;
-            //結果はstrに入っている
-            
-        }
+            //結果はstrに入っている          
+            count += 1;
+        },
 
     }
 
