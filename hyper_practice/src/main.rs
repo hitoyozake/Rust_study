02530b1a8161d838308c569b7fe39f6188703be3
hitoyozake <mikoto_1122@yahoo.co.jp>
2017-11-15 07:32:15 +0900

    古いhyperの記述

--- hyper_practice/src/main.rs
+++ hyper_practice/src/main.rs
@@ -6,6 +6,15 @@ fn main() {
     use tokio_core::reactor::Core;
     let URL = "http://http2bin.org.get";
     let core = Core::new();
+
+    let client = Client::new();
+
+    let res = client.get(URL).send().unwrap();
+
+    match res {
+        hyper::Ok => println!("ok"),
+        _ => println!("failed"),
+    }
     
     println!("Hello, world!");
 }
