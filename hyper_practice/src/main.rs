44ba79de503d0c0ebc4d35f862e8e2c7e7b75858
hitoyozake <mikoto_1122@yahoo.co.jp>
2017-11-16 06:42:20 +0900

    hyperのバージョンを指定．unwrapはresult型に対する乱暴な使い方なので今度ちゃんとした方法を学ぶべき

--- hyper_practice/src/main.rs
+++ hyper_practice/src/main.rs
@@ -1,20 +1,23 @@
 extern crate hyper;
 extern crate tokio_core;
+extern crate futures;
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::Client;
+use tokio_core::reactor::Core;
 
 fn main() {
     use hyper::Client;
     use tokio_core::reactor::Core;
-    let URL = "http://http2bin.org.get";
-    let core = Core::new();
+    let URL = "http://http2bin.org.get".parse().unwrap();
 
-    let client = Client::new();
 
-    let res = client.get(URL).send().unwrap();
+    let mut core = Core::new().unwrap();
 
-    match res {
-        hyper::Ok => println!("ok"),
-        _ => println!("failed"),
-    }
+    let client = Client::new(&core.handle());
+
+    let work = client.get(URL);
     
     println!("Hello, world!");
 }
