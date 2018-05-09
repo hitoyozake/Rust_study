7bc9ac80082e055e840125e18bcb4b6f07976ba0
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-05-10 06:59:45 +0900

    vecdec

--- hyper_practice/src/main.rs
+++ hyper_practice/src/main.rs
@@ -10,14 +10,24 @@ use tokio_core::reactor::Core;
 fn main() {
     use hyper::Client;
     use tokio_core::reactor::Core;
-    let URL = "http://http2bin.org.get".parse().unwrap();
+    let URL = "http://wa3.i-3-i.info/word11871.html".parse().unwrap();
 
 
     let mut core = Core::new().unwrap();
 
     let client = Client::new(&core.handle());
 
-    let work = client.get(URL);
+    let work = client.get(URL).and_then(|res|{
+        println!("Response: {}", res.status());
+        println!("Headers: \n{}", res.headers());
+
+        res.body().for_each(|chunk|{
+            io::stdout().write_all(&chunk).map_err(From::from)
+        })
+    }).map(|_|{
+        println!("\n\nDone");
+    });
+
+    core.run(work).unwrap();
     
-    println!("Hello, world!");
 }
