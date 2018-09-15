333311dde1e47ae54b4087a95b867e37143203a7
hitoyozake <mikoto_1122@yahoo.co.jp>
2018-09-15 20:57:35 +0900

    dylink

--- callDxLib/src/main.rs
+++ callDxLib/src/main.rs
@@ -1,3 +1,14 @@
+#[link(name="DxLib_x64.dll")]
+#[no_mangle]
+extern "stdcall" {
+    fn dx_DxLib_Init()->i32;
+    fn dx_DxLib_End()->i32;
+}
+
+
 fn main() {
+
+    unsafe {dx_DxLib_Init();}
     println!("Hello, world!");
+    unsafe {dx_DxLib_End();}
 }
