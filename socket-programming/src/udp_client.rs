4637e393211902926df8ab2d1cc08f124b609624
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-11-02 15:52:38 +0900

    ok

--- socket-programming/src/udp_client.rs
+++ socket-programming/src/udp_client.rs
@@ -1,21 +1,22 @@
 use std::net::UdpSocket;
 use std::{io, str};
 
-pub fn communication(address: &str) -> Result<(), failure::Error>{
-    let socket = UdpSocket.bind("127.0.0.1:0").expected("can't binded at udp client");
+pub fn communicate(address: &str) -> Result<(), failure::Error>{
+    let socket = UdpSocket::bind("127.0.0.1:27871").expect("can't binded at udp client");
 
     loop {
         let mut input = String::new();
 
         io::stdin().read_line(&mut input)?;
+        socket.send_to(input.as_bytes(), address)?;
 
-        let mut buf = [0u8, 1024];
+        let mut buffer = [0u8; 1024];
 
         socket.recv_from(&mut buffer).expect("failed to receive");
 
         print!(
             "{}",
-            str::from_utf8(&buffer).expect("failed to convert to String");
+            str::from_utf8(&buffer).expect("failed to convert to String")
         );
     }
 }
