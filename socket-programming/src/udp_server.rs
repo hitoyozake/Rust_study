e2846d3b03f8fe83386d17381a6824cad4c7cc7d
hitoyozake <mikoto_1122@yahoo.co.jp>
2019-11-02 14:36:13 +0900

    commit

--- socket-programming/src/udp_server.rs
+++ socket-programming/src/udp_server.rs
@@ -2,6 +2,24 @@ use std::io::{Read, Write};
 use std::net::{UdpSocket};
 use std::{str, thread};
 
+// reference implementation
+pub fn serve2(address: &str)->Result<(), failure::Error> {
+
+    let server_socket = UdpSocket::bind(address)?;
+
+    loop {
+        let mut buffer = [ 0u8; 1024];
+
+        let (size, src) = server_socket.recv_from(&mut buf)?;
+
+        debug! ("Handling data from {}", src)
+
+        print!("{}", str::from_utf8(buf[..size])?);
+        server_socket.send_to(&buf, src)?;
+
+    }
+}  
+
 
 
 pub fn serve(address: &str)->Result<(), failure::Error>{
