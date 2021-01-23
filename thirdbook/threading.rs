4e28170333bd94127ff1e6eeb769540a730fee8f
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-23 20:16:17 +0900

    threading2

--- thirdbook/threading.rs
+++ thirdbook/threading.rs
@@ -1,6 +1,31 @@
 use std::thread;
+//use std::rc::Rc; // for single threading Reference Count
+use std::sync::{Arc, Mutex}; // これは use std::sync::Mutexとuse std::sync::Arcに等しい
 
 fn main(){
+    let mut handles = Vec::new();
+
+    let mut data = Arc::new(Mutex::new(vec![1; 10]));
+
+    for x in 0..10{
+        let data_ref = data.clone();
+        handles.push(thread::spawn(move||{
+            let mut d = data_ref.lock().unwrap();
+            d[x] += 1;
+        }))
+    }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+
+    dbg!(data);
+
+}
+
+
+
+fn main2(){
     let mut handles = Vec::new();
 
     for x in 0..10{
