ce93f482e3b503ea5b74bd433900412c04086541
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-23 20:20:25 +0900

    messagePassing

--- thirdbook/threading.rs
+++ thirdbook/threading.rs
@@ -2,7 +2,23 @@ use std::thread;
 //use std::rc::Rc; // for single threading Reference Count
 use std::sync::{Arc, Mutex}; // これは use std::sync::Mutexとuse std::sync::Arcに等しい
 
+// message passing
+use std::sync::mpsc;
 fn main(){
+    let (tx, rx) = mpsc::channel();
+    let handle = thread::spawn(move || {
+        let data = rx.recv().unwrap();
+        println!("{}", data);
+    });
+
+    let _ = tx.send("Hello, world!");
+
+    let _ = handle.join();
+}
+
+
+// 共有メモリ
+fn main3(){
     let mut handles = Vec::new();
 
     let mut data = Arc::new(Mutex::new(vec![1; 10]));
