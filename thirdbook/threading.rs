b323dbf06796976ee1eaac970ee08c87ae943bec
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-23 20:42:24 +0900

    ok

--- thirdbook/threading.rs
+++ thirdbook/threading.rs
@@ -5,18 +5,42 @@ use std::sync::{Arc, Mutex}; // これは use std::sync::Mutexとuse std::sync::
 // message passing
 use std::sync::mpsc;
 fn main(){
-    let (tx, rx) = mpsc::channel();
+    let mut handles = Vec::new();
 
-    //このタイミングでsendしても問題なし．キューに追加される
-    let _ = tx.send("Hello, world!");
+    let mut data = vec![1; 10];
+    let mut snd_channels = Vec::new();
+    let mut rcv_channels = Vec::new();
 
-    let handle = thread::spawn(move || {
-        let data = rx.recv().unwrap();
-        println!("{}", data);
-    });
+    for _ in 0..10 {
+        let (snd_tx, snd_rx) = mpsc::channel();
+        let (rcv_tx, rcv_rx) = mpsc::channel();
+
+        snd_channels.push(snd_tx);
+        rcv_channels.push(rcv_rx);
+
+        handles.push(thread::spawn(move||{
+            let mut data = snd_rx.recv().unwrap();
+            data += 1;
+            let _ = rcv_tx.send(data);
+        }));
+    }
+
+    for x in 0..10{
+        let _ = snd_channels[x].send(data[x]);
+    }
+
+    for x in 0..10{
+        data[x] = rcv_channels[x].recv().unwrap();
+    }
+
+    for handle in handles{
+        let _ = handle.join();
+    }
+
+    dbg!(data);
+    
 
 
-    let _ = handle.join();
 }
 
 
