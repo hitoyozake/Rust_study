cf184460523259b0f468bae2e9104ad9ca6c81ad
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-23 18:23:57 +0900

    commit

--- thirdbook/mvsemantics.rs
+++ thirdbook/mvsemantics.rs
@@ -3,6 +3,7 @@ fn main(){
     let mut important_data = "Hello, world!".to_string();
     //rustの = は原則 movesemantics. C++ は copy semanticsなので基本コピーとしての意味論になるが，rustはmoveになる
     important_data = calc_data(important_data); // 醤油うけんを私，返してもらう
+    calc_dataref(&important_data);
 
     println!("{}", important_data);
 }
@@ -14,5 +15,7 @@ fn calc_data(data: String) -> String {
 }
 
 // 参照にするとmoveではなくなる
-
+fn calc_dataref(data: &String){
+    println!("{} @ calcDataRef", data);
+}
 
