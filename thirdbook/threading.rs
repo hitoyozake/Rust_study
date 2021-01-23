1b37f6640400b56c9102c4a6b6c7a82968699dbe
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-23 19:18:07 +0900

    基本クロージャは参照渡しなのでライフタイムに注意

--- thirdbook/threading.rs
+++ thirdbook/threading.rs
@@ -1,7 +1,19 @@
 use std::thread;
 
 fn main(){
-    let hnd = thread::spawn(||{
+    let mut handles = Vec::new();
+
+    for x in 0..10{
+        handles.push(thread::spawn(move||{
+            println!("Hello, world!; {}", x);
+        }));
+    }
+
+        for handle in handles{
+            let _ = handle.join();
+        }
+
+    let hnd = thread::spawn( ||{
         println!("thread");
     });
 
