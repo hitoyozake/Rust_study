760d89fb20ea4e942879e1c2df3bee75feb97235
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-30 16:09:26 +0900

    commit

--- thirdbook/ac073c.rs
+++ thirdbook/ac073c.rs
@@ -9,6 +9,22 @@ fn read<T: std::str::FromStr>() -> T {
 
 fn main(){
 
-    let n = read<String>();
+    let n = read::<String>().parse::<i64>().ok().unwrap();
+    let mut hashMap = HashMap::<i64, i64>::new();
+    for i in 0..n{
+        let x = read::<String>().parse::<i64>().ok().unwrap();
+        let counter = hashMap.entry(x).or_insert(0);
+        *counter += 1;
+    }
+
+    let cnt = hashMap.iter().filter(|num|{num.1%2==1});
+    let mut result = 0;
+    for i in cnt{
+        result += 1;
+    }
+
+    println!("{}", result);
+
+
 
 }
