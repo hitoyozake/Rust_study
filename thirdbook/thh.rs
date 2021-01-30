fdc2e87ba07de1a273b5246a642bbd77890eb09b
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-30 15:02:42 +0900

    ok

--- thirdbook/thh.rs
+++ thirdbook/thh.rs
@@ -18,9 +18,17 @@ impl Area for Triangle{
     }
 }
 
+//tupleの練習
+fn make_tuple<T, U>(a: T, b: U)->(T, U){
+    return (a, b)
+}
 
 fn main(){
     println!("print");
     let a = Triangle{ a: Point{x:32, y:12}, b: Point{x:24, y:12},c: Point{x:24, y:16}};
     println!("{}", a.calcArea());
+
+    let t = make_tuple( 1, 2 );
+
+    println!("t.0: {}, t.1: {}", t.0, t.1);
 }
