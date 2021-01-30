dccaaeb4b046be46fe977e7108a20ad78c86dbfd
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-01-30 15:33:10 +0900

    073時はじめ

---
+++ thirdbook/ac073c.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+
+fn main(){
+
+    let n = read<String>();
+
+}
