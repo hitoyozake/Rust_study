987ed113b2c144517447d4393932de5bd95beb8d
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-02-19 19:59:32 +0900

    clap_alternative

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,7 +1,25 @@
 use std::env;
-use clap::{App, Arg};
+use clap::{App, Arg, Clap};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+    
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
 
 fn main(){
+    /*
     //builders pattern
     let matches = App::new("My RPN progr")
             .version("1.0.0")
@@ -19,14 +37,22 @@ fn main(){
                 .value_name("FILE")
                 .index(1)
                 .required(false) 
-            ).get_matches();
-
+            ).
+            get_matches();
+            
         match matches.value_of("formula_file"){
             Some(file) => println!("File specified: {}", file),
             None => println!("No file specified."),
         }
         let verbose = matches.is_present("verbose");
         println!("Is verbosity specified?: {}", verbose)
+    */
+    let opts = Opts::parse();
+    match opts.formula_file{
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified"),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
 
 
