640b8f48f0a856ee8bd4817fb8b6e674070256a3
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-02-19 19:39:34 +0900

    clap

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -2,29 +2,30 @@ use std::env;
 use clap::{App, Arg};
 
 fn main(){
+    //builders pattern
     let matches = App::new("My RPN progr")
             .version("1.0.0")
             .author("Your name")
             .about("Super awesome sample RPN calculator")
             .arg(
-                Arg::with_name("verbose")
+                Arg::new("verbose")
                 .about("Sets the level of verbosity")
                 .short('v')
                 .long("verbose")
                 .required(false)
             ).arg(
-                Arg::with_name("formula_file")
+                Arg::new("formula_file")
                 .about("Formulas written in RPN")
                 .value_name("FILE")
                 .index(1)
                 .required(false) 
             ).get_matches();
 
-        match maches.value_of("formula_file"){
+        match matches.value_of("formula_file"){
             Some(file) => println!("File specified: {}", file),
             None => println!("No file specified."),
         }
-        let verbose = matches.in_present("verbose");
+        let verbose = matches.is_present("verbose");
         println!("Is verbosity specified?: {}", verbose)
 }
 
