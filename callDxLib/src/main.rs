591b716a0469c3edaa292c3778559e4be928ebb0
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-02-23 18:33:02 +0900

    コマンド実行テスト完了

--- callDxLib/src/main.rs
+++ callDxLib/src/main.rs
@@ -1,14 +1,26 @@
-#[link(name="DxLib_x64.dll")]
+
+#[link(name="DxLib_x64_d", kind="static")]
 #[no_mangle]
-extern "stdcall" {
-    fn dx_DxLib_Init()->i32;
-    fn dx_DxLib_End()->i32;
+extern {
+    fn DxLib_Init()->i32;
+    fn DxLib_End()->i32;
 }
 
 
 fn main() {
-
-    unsafe {dx_DxLib_Init();}
+    unsafe {DxLib_Init();}
     println!("Hello, world!");
-    unsafe {dx_DxLib_End();}
+    //unsafe {DxLib_End();}
 }
+
+/*
+#[link(name="foo", kind="static")]
+extern {
+    fn hoge();
+}
+
+fn main()
+{
+    unsafe{hoge();}
+}
+*/
--- compress_image/src/main.rs
+++ compress_image/src/main.rs
@@ -1,3 +1,8 @@
+use std::process::Command;
+
 fn main() {
+     let output = Command::new("ls")
+     .output().expect("failed to run command");
+    println!("{}", String::from_utf8_lossy(&output.stdout));
     println!("Hello, world!");
 }
