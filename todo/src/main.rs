55b120032fb732fdd28cb7ee9d215ed274f7600a
hitoyozake <mikoto_1122@yahoo.co.jp>
2021-03-14 21:43:50 +0900

    commit

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,12 @@
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/")]
+async fn index() -> Result<HttpResponse, actix_web::Error> {
+    let response_body = "Hello world!";
+
+    Ok(HttpResponse::Ok().body(response_body))
+}
+
 fn main() {
     println!("Hello, world!");
 }
